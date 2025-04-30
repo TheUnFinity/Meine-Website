e12964dc132afa18690a2b90b6ed3b5685c1dd7e
Marcel K. <106357974+Ka1serM@users.noreply.github.com>
2025-04-30 21:48:19 +0200

    delete components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Footer from '../components/Footer.svelte';
-    import Header from '../components/Header.svelte';
     import '../app.css';
 
     import { navigation } from '$lib/data.js';
