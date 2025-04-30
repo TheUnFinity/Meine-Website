59e5eb3b4e65627666f04ef5d45c730ef54c0561
Marcel K. <106357974+Ka1serM@users.noreply.github.com>
2025-04-30 22:01:11 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import NavBar from '../components/Figma/NavBar.svelte';
-    import HeroSection from '../components/Figma/HeroSection.svelte';
-    import AboutMe from '../components/Figma/AboutMe.svelte';
-    import RecentWork from '../components/Figma/RecentWork.svelte';
-    import Contact from '../components/Figma/Contact.svelte';
+    import NavBar from '$components/Figma/NavBar.svelte';
+    import HeroSection from '$components/Figma/HeroSection.svelte';
+    import AboutMe from '$components/Figma/AboutMe.svelte';
+    import RecentWork from '$components/Figma/RecentWork.svelte';
+    import Contact from '$components/Figma/Contact.svelte';
 </script>
 
 <main>
--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,16 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: "/theunfinity.github.io"  //GitHub Pages base path
-		  },
-		alias: {
-			"@/*": "./path/to/lib/*",
-		  },
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: "/theunfinity.github.io"  // Required for GitHub Pages
+    },
+    alias: {
+      $components: "src/components",
+      $lib: "src/lib"
+    }
+  }
 };
 
 export default config;
